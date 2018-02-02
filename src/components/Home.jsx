215991446f3dfeba2f972b5bc7346033b9eda188
jgalianoz <manuelgalindez81@gmail.com>
2018-02-02 00:33:55 -0400

    [add] styles MainSection-Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,39 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const MainSection = styled.section`
+const MainSection = styled.div`
   background-image: url('http://localhost:3001/assets/images/mainSection.jpg');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   min-height: 600px;
+  color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  .containerButton {
+    margin-top: 20px;
+  }
 `;
 
 const Title = styled.h1`
-  margin: 0;
   padding: 0;
+  font-size: 48px;
+  color: #FFFFFF;
+`;
+
+const Link = styled.a`
+  padding: 18px 48px 16px;
+  border-radius: 500px;
+  color: #FFFFFF;
+  background-color: #1db954;
+  text-decoration: none;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 14px;
+  font-weight: 700;
+  display: inline-block;
 `;
 
 const Home = () => (
@@ -21,10 +43,10 @@ const Home = () => (
       <Title>
         Música para todos
       </Title>
-      <div>
-        <a href="#">
+      <div className="containerButton">
+        <Link href="#">
           Consigue tu musica favorita
-        </a>
+        </Link>
       </div>
     </MainSection>
   </section>
